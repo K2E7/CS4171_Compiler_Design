a23b0608bd7c39e90c099590e9f0d8f180464667
K2E7 <k2e7@outlook.com>
2023-08-11 11:08:03 +0530

    modified.

--- Lab1_Lexical_Analyzer/input.c
+++ Lab1_Lexical_Analyzer/input.c
@@ -5,15 +5,15 @@
 int main()
 {
     int x = 10;
-    float y = 3.14;
+    float y_x = 3.14;
     if (x > 0)
     {
         /* Can You Hear Me
    As I say your Name*/
-        while (y >= 1)
+        while (y_x >= 1)
         {
             printf("Hello, world!\n");
-            y = y - 1;
+            y_x = y_x - 1;
         }
     }
     return 0;
